747ad6887d52953f8987650288b12a4810df2273
Gilvan Izidio dos Santos <izidio.dev.work@gmail.com>
2026-03-20 16:38:46 -0300

    fix: connection db on production

--- src/shared/infra/database/data-source.ts
+++ src/shared/infra/database/data-source.ts
@@ -1,14 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 const options: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'ms_geofence_admin',
+  url: process.env.DB_URL || 'localhost',
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
   synchronize: false,
-  logging: process.env.NODE_ENV === 'development',
+  logging: isDevelopment,
   entities: ['src/modules/**/entities/*.entity.{ts,js}'],
   migrations: ['src/shared/infra/database/migrations/*.{ts,js}'],
 };
